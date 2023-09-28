4f73a204b1ecd44853be4c1f578a93e3330899ad
HashimAbdirahim <51890368+HashimAbdirahim@users.noreply.github.com>
2023-09-28 10:32:56 -0700

    Pushed Files for assignment

---
+++ js/script.js
@@ -0,0 +1,106 @@
+// STEP 1
+let someMonth
+function theMonth(){}
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+// STEP 2
+100
+"String literal"
+true
+null
+// STEP 3
+x = 12
+y = 10
+x + y
+x * 12
+
+// STEP 4
+firstName
+lastName
+address
+strCity
+strState
+strZipCode
+iYourAge
+referralSource
+mayWeContactYou
+
+// STEP 5
+firstName = "Hashim"
+lastName = "Abdirahim"
+strState = "CA"
+
+// STEP 6
+x = 12 + "a number"
+console.log(x)
+
+// STEP 7
+let example1 = true + "a boolean"
+console.log(example1)
+let example2 = 12 + false
+console.log(example2)
+
+//STEP 8
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString)
+
+// STEP 9 
+let var1 = null
+console.log(x)
+let var2
+console.log(var2)
+
+// STEP 10
+console.log(typeof 'string')
+console.log(typeof 16)
+console.log(typeof true)
+console.log(typeof String)
+console.log(typeof undeclaredVariable)
+
+// STEP 11
+alert('Hello' + 'Hashim Abdirahim' + ', welcome to the JavaScript class!')
+
+// STEP 12
+let name = 'Hashim Abdirahim'
+alert('Hello' + name + ', welcome to the JavaScript class!')
+
+// STEP 13
+let course = 'JavaScript'
+alert('Hello' + name + ', welcome to the ' + course + ' class!')
+
+// STEP 14
+alert('Hello' + name + '.' + '\nWelcome to the ' + course + ' class!')
+
+// STEP 15
+name = prompt('What is your name?')
+
+// STEP 16
+course = prompt('What class are you taking?')
+
+// STEP 17
+x = 10
+y = 20
+console.log(x+y)
+
+// STEP 18
+x = 20
+x += 20
+console.log(x)
+
+// STEP 19
+x = 20
+x *= 5
+console.log(x)
+
+// STEP 20
+x = 20 % 3
+x /= 1
+console.log(x)
+
+// STEP 21
+console.log((x == 2 || x < 100))
+
+// STEP 22
+console.log((x <= 12 && x != 2))
